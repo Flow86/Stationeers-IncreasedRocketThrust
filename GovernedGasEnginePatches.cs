25fb438d699b10880adc30712b156b91df55f033
Flow86 <656249+Flow86@users.noreply.github.com>
2024-09-28 16:44:49 +0200

    Add Patch for Pumped Gas Engine

---
+++ GovernedGasEnginePatches.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Objects.Pipes;
+using HarmonyLib;
+
+namespace RocketThrustOverride.Patches
+{
+    internal class GovernedGasEnginePatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GovernedGasEngine))]
+        [HarmonyPatch("EngineEfficiency", MethodType.Getter)]
+        public static bool EngineEfficiency_Prefix(GovernedGasEngine __instance, ref float __result)
+        {
+            __result = 50f;
+            return false;
+        }
+    }
+}
--- RocketThrustOverride.cs
+++ RocketThrustOverride.cs
@@ -12,7 +12,7 @@ namespace RocketThrustOverride
             UnityEngine.Debug.Log("RocketThrustOverride: Hello!");
 
             Harmony harmony = new Harmony("RocketThrustOverridePatches");
-            harmony.PatchAll(typeof(InventoryWindowPatches));
+            harmony.PatchAll(typeof(GovernedGasEnginePatches));
 
             UnityEngine.Debug.Log("RocketThrustOverride: Done!");
         }
