07667657a786624d3b94c6c4bb1dbe2c263f3bbe
Flow86 <656249+Flow86@users.noreply.github.com>
2024-09-28 15:36:27 +0200

    import project

---
+++ README.md
@@ -0,0 +1,4 @@
+﻿# Fix Window Gaps
+
+This mod for Stationeers fixes the weird gaps after opening and closing inventory windows
+
---
+++ RocketThrustOverride.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using StationeersMods.Interface;
+
+using RocketThrustOverride.Patches;
+
+namespace RocketThrustOverride
+{
+    class RocketThrustOverride : ModBehaviour
+    {
+        public override void OnLoaded(ContentHandler contentHandler)
+        {
+            UnityEngine.Debug.Log("RocketThrustOverride: Hello!");
+
+            Harmony harmony = new Harmony("RocketThrustOverridePatches");
+            harmony.PatchAll(typeof(InventoryWindowPatches));
+
+            UnityEngine.Debug.Log("RocketThrustOverride: Done!");
+        }
+    }
+}
